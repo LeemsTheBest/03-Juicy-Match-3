34ebddb181c0e73bbd1ace96b056310013a6b83a
LeemsTheBest <77755513+LeemsTheBest@users.noreply.github.com>
2021-03-10 20:44:17 -0500

    Juicy Match 3
    Juicy Match 3 Project

---
+++ Pieces/Piece.gd
@@ -0,0 +1,66 @@
+extends Node2D
+
+export (String) var color
+var is_matched
+var is_counted
+var selected = false
+var target_position = Vector2(0,0)
+var default_modulate = Color(1,1,1,1)
+var highlight = Color(1,0.8,0,1)
+
+export var fall_speed = 1.0
+
+var dying = false
+
+func _ready():
+	randomize()
+
+func _physics_process(_delta):
+	if dying and not $Tween.is_active():
+		queue_free()
+	if selected:
+		$Selected.emitting = true
+		$Select.show()
+		z_index = 10
+	else:
+		$Selected.emitting = false
+		$Select.hide()
+		z_index = 1
+
+func move_piece(change):
+	target_position = position + change
+	position = target_position
+
+func die():
+	dying = true
+	var target_color = $BubbleTile.modulate
+	target_color.s = 1
+	target_color.h = randf()
+	target_color.a = 0.25
+	var fall_duration = randf()*fall_speed + 1
+	var rotate_amount = (randi() % 1440) - 720
+	
+	var target_pos = position
+	target_pos.y = 1100
+	$Tween.interpolate_property(self, "position", position, target_pos, fall_duration, Tween.TRANS_CUBIC, Tween.EASE_IN)
+	$Tween.start()
+	$Tween.interpolate_property($BubbleTile, "modulate", $BubbleTile.modulate, target_color, fall_duration-0.25, Tween.TRANS_EXPO, Tween.EASE_IN)
+	$Tween.start()
+	$Tween.interpolate_property(self, "rotation_degrees", rotation_degrees, rotate_amount, fall_duration-0.25, Tween.TRANS_QUINT, Tween.EASE_IN)
+	$Tween.start()
+
+	if color == "Bubble":
+		get_node("/root/Game/Bubble").playing = true
+	if color == "Fire":
+		get_node("/root/Game/Fire").playing = true
+	if color == "Leaf":
+		get_node("/root/Game/Leaf").playing = true
+	if color == "Lightning":
+		get_node("/root/Game/Lightning").playing = true
+	if color == "Earth":
+		get_node("/root/Game/Earth").playing = true
+	if color == "Star":
+		get_node("/root/Game/Star").playing = true
+	if color == "Sky":
+		get_node("/root/Game/Sky").playing = true
+	
---
+++ UI/EndGame.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _ready():
+	$Label.text = "Thanks for playing!\n\nLevel 1 Score Was " + str(Global.level1_score) + "\n\nLevel 2 Time Was " + str(Global.level2_time)
+
+
+func _on_Play_pressed():
+	Global.score = 0
+	Global.time = Global.level1_default
+	Global.level = 1
+	Global.level1score = 0
+	Global.level2_time = 0
+	get_tree().change_scene("res://Game.tscn")
+
+func _on_Quit_pressed():
+	get_tree().quit()
